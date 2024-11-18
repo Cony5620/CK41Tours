95c97efd3898f2165f702ea5e99cc2c367766a1b
am-mt <ammt.thu@gmail.com>
2024-11-18 12:23:08 +0700

    TT commit

---
+++ CK41Tours/Repositories/Domain/ITTRepository.cs
@@ -0,0 +1,9 @@
+﻿using CK41Tours.Models.DataModels;
+using CK41Tours.Repositories.Common;
+/// ammt 20241117
+namespace CK41Tours.Repositories.Domain
+{
+    public interface ITTRepository:IBaseRepository<TTEntity>
+    {
+    }
+}
---
+++ CK41Tours/Services/ITTService.cs
@@ -0,0 +1,13 @@
+﻿using CK41Tours.Models.ViewModels;
+
+namespace CK41Tours.Services
+{
+    public interface ITTService
+    {
+        void Create(TTViewModel TTViewModel,string photoUrl);
+        IEnumerable<TTViewModel> GetAll();
+        TTViewModel GetBy(string Id);
+        void Update(TTViewModel TTViewModel, string photoUrl);
+        bool Delete(string Id);
+    }
+}
