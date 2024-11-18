e79440161fccc7c6b77a131f8c083bb4852fe830
Khaing Zin Htet <khaingzinhtet562000@gmail.com>
2024-11-18 14:33:56 +0630

    asdf

--- CK41Tours/obj/Debug/net8.0/CK41Tours.AssemblyInfo.cs
+++ CK41Tours/obj/Debug/net8.0/CK41Tours.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CK41Tours")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+867709567f114cd369db593c636680fdcd70ffee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deda93a0cd447484e59de6d85076791b6f3a7087")]
 [assembly: System.Reflection.AssemblyProductAttribute("CK41Tours")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CK41Tours")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
